302c3ff543f860a81bc6d3f749543d6d478ae6b9
cnievaslozano <cnievaslozano@gmail.com>
2023-11-22 17:25:44 +0100

    migrations programacions

---
+++ database/migrations/2023_11_22_161952_create_programacions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('programacions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('users_id'); // Relación con la tabla users
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->string('any_academic');
+            $table->text('descripcio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('programacions');
+    }
+};
